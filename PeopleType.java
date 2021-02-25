febb10d3aef60332b8d86cf3963fdeefcaedb646
Kyle Smilon <smilonk@moravian.edu>
2021-02-25 10:25:53 -0500

    Added Java Doc to PeopleType.java

--- PeopleType.java
+++ PeopleType.java
@@ -1,5 +1,10 @@
 package Project01;
 
+/**
+ * Using enum this class represents
+ * the different people located inside of a tribe.
+ * Each person has a description of their role in the game.
+ */
 public enum PeopleType
 {
     warrior ("Solder"),
@@ -8,12 +13,19 @@ public enum PeopleType
 
     private String description;
 
+    /**
+     *
+     * @param types takes the current people type and sets the associated
+     *              description along with that type.
+     */
     PeopleType (String types)
     {
         description = types;
     }
 
-
+    /**
+      * @return the description of the people type.
+     */
     public String getDescription()
     {
         return description;
